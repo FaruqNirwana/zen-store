8be5c1eeb1e6ccfd0d862c63d8d316493767254a
Faruq <umarulfaruqq123@gmail.com>
2025-11-18 09:45:07 +0700

    fixbug2

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,17 @@
-import { serve } from "inngest";
-import {
-  inngest,
-  syncUserCreation,
-  syncUserDelete,
-  syncUserUpdate,
-} from "@/config/inngest";
+import { serve } from "inngest/next";
+import { inngest } from "@/config/inngest";
+
+import { 
+  syncUserCreation, 
+  syncUserDelete, 
+  syncUserUpdate 
+} from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncUserCreation, syncUserUpdate, syncUserDelete],
+  functions: [
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserDelete,
+  ],
 });
